72cce0b1bbd6a6e20df7be60c43cf0be266b4145
calebgood <50765275+calebgood@users.noreply.github.com>
2019-09-08 18:19:04 +0530

    Create nth roots.py

---
+++ nth roots.py
@@ -0,0 +1,22 @@
+#Last digit is arbitary and may vary
+def solve(a, b,n=2):
+  num=b
+  while True:
+    m = (a + b)/2
+    if abs(m**n-num)<=0.000000001:
+      m=float('%.8f'%m)
+      if (m*10)%10==0:
+        return int(m)
+      else:
+        return m
+    elif m**n-num<=0:
+      a = m
+    else:
+      b = m
+  
+
+if __name__=='__main__':
+  num=int(input('Enter a number:'))
+  print(solve(0,num,3))
+  print('By default the sqrt function is activated.\nYou can change that by changing the arguments')
+  
